f749ccb29e878bf73768e003168b5a0a0725befb
gabrieldemian <gabrielgcr45@gmail.com>
2025-12-16 22:29:34 +0100

    io uring setup

--- src/io_uring.zig
+++ src/io_uring.zig
@@ -1,7 +1,4 @@
 const std = @import("std");
-const Io = std.Io;
-const Allocator = std.mem.Allocator;
-const net = std.net;
 const posix = std.posix;
 const linux = std.os.linux;
 const page_size_min = std.heap.page_size_min;
@@ -15,19 +12,6 @@ pub fn main() !void {
     var sq = try std.Deque(u8).initCapacity(gpa, page_size_min);
     defer sq.deinit(gpa);
     const sqmmap = try posix.mmap(
-        sq.buffer.ptr,
-        page_size_min,
-        posix.PROT.READ | posix.PROT.WRITE,
-        .{ .TYPE = .SHARED, .ANONYMOUS = true },
-        -1,
-        0,
-    );
-    defer std.posix.munmap(sqmmap);
-
-    // request queue
-    var rq = try std.Deque(u8).initCapacity(gpa, page_size_min);
-    defer rq.deinit(gpa);
-    const rqmmap = try posix.mmap(
         null,
         page_size_min,
         posix.PROT.READ | posix.PROT.WRITE,
@@ -35,21 +19,31 @@ pub fn main() !void {
         -1,
         0,
     );
-    defer std.posix.munmap(rqmmap);
+    defer posix.munmap(sqmmap);
 
-    // const params = linux.io_uring_params{
-    //     .sq_entries = 1,
-    //     .cq_entries = 1,
-    //     .flags = 2,
-    //     .sq_thread_cpu = 2,
-    //     .sq_thread_idle = 2,
-    //     .features = 2,
-    //     .wq_fd = 2,
-    //     .resv = [3]u32{1,2,3},
-    // .sq_off= io_sqring_offsets,
-    // .cq_off= io_cqring_offsets,
-    // };
-    // const entries: u32 = 2;
-    // const ur = linux.io_uring_setup(entries, &params);
-    // _ = ur;
+    // completion queue
+    var cq = try std.Deque(u8).initCapacity(gpa, page_size_min);
+    defer cq.deinit(gpa);
+    const cqmmap = try posix.mmap(
+        null,
+        page_size_min,
+        posix.PROT.READ | posix.PROT.WRITE,
+        .{ .TYPE = .SHARED, .ANONYMOUS = true },
+        -1,
+        0,
+    );
+    defer posix.munmap(cqmmap);
+
+    // initialize a zeroed array and cast to a struct
+    var pp: [@sizeOf(linux.io_uring_params)]u8 align(@alignOf(linux.io_uring_params)) = undefined;
+    @memset(&pp, 0);
+    const p: *linux.io_uring_params = @ptrCast(&pp);
+
+    // The io_uring_setup(2) system call sets up a submission queue (SQ)
+    // and completion queue (CQ) with at least entries entries, and
+    // returns a file descriptor which can be used to perform subsequent
+    // operations on the io_uring instance.
+    const ring_fd = linux.io_uring_setup(1, p);
+    std.debug.print("ring_fd {any}\n", .{ring_fd});
+    std.debug.print("p {any}\n", .{p.*});
 }
