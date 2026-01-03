3f6c3d37f7c6b80efc5cfa60b5e81be1e0d2d1d4
gabrieldemian <gabrielgcr45@gmail.com>
2026-01-03 01:13:27 +0100

    bencode: added struct

--- src/bencode/root.zig
+++ src/bencode/root.zig
@@ -3,31 +3,27 @@ const Io = std.Io;
 const print = std.debug.print;
 const assert = std.debug.assert;
 
-const MyData = struct {
+pub const MyData = struct {
     myint: u8,
 };
 
 pub const Error = error{
     MalformedBuffer,
+
     FoundNonInt,
     UnexpectedSign,
     NotSupported,
     LeadingZero,
-};
 
-const State = enum {
-    None,
-    Integer,
-    NegInteger,
-    List,
-    Dict,
-    StringLen,
+    NoLen,
+    NoNegative,
+    WrongLen,
 };
 
 pub fn decode(
     comptime T: type,
     w: *Io.Writer,
-    data: []const u8,
+    comptime data: anytype,
 ) !void {
     const info = @typeInfo(T);
 
@@ -36,12 +32,60 @@ pub fn decode(
             const r = try decode_int(T, data);
             _ = try w.write(std.mem.asBytes(&r));
         },
+        inline .pointer => {
+            // todo: how to check if pointer is []u8 ?
+            if (info.pointer.alignment != 1 and info.pointer.sentinel_ptr == null) {
+                return Error.MalformedBuffer;
+            }
+            const r = try decode_str(data);
+            if (r.len == 0) {
+                _ = try w.writeByte(0);
+            } else {
+                _ = try w.write(r);
+            }
+        },
+        inline .@"struct" => {
+            // initialize the struct with zeroes, loop over the fields,
+            // and call `decode` recursively with the field type.
+            var val: T = std.mem.zeroes(T);
+            inline for (std.meta.fields(T)) |f| {
+                @field(val, f.name) = return decode(f.type, w, data);
+            }
+        },
         else => return Error.NotSupported,
     };
 }
 
-/// Decode an integer, for example:
-/// `i123e`
+/// Decode a byte str, encoded as `<length>:<contents>`.
+/// `<length>` is specified in bytes, not characters.
+/// For example: `6:italia`.
+pub fn decode_str(data: []const u8) ![]const u8 {
+    if (data.len < 2) {
+        return Error.MalformedBuffer;
+    }
+
+    if (data[0] == '-') {
+        return Error.NoNegative;
+    }
+
+    // empty string
+    if (std.mem.eql(u8, data, "0:")) {
+        return "";
+    }
+
+    const colon = std.mem.find(u8, data, ":") orelse return Error.MalformedBuffer;
+    const len = try std.fmt.parseInt(usize, data[0..colon], 10);
+    const str = data[colon + 1 ..];
+
+    if (len != str.len) {
+        return Error.WrongLen;
+    }
+
+    return str;
+}
+
+/// Decode an integer, encoded as `i<base10 integer>e`.
+/// For example: `i123e`
 pub fn decode_int(
     comptime T: type,
     data: []const u8,
@@ -101,6 +145,44 @@ pub fn decode_int(
 
 const expect = std.testing.expect;
 var buffer: [8]u8 = undefined;
+
+test "struct" {
+    const encoded = "i4e";
+    var w = std.Io.Writer.fixed(&buffer);
+    try decode(MyData, &w, encoded);
+    std.debug.print("my_data {any}\n", .{buffer});
+    try expect(buffer[0] == 4);
+}
+
+test "empty_str" {
+    const encoded = "0:";
+    var w = std.Io.Writer.fixed(&buffer);
+    try decode(@TypeOf(encoded), &w, encoded);
+    try expect(buffer[0] == 0);
+}
+
+test "decode_str" {
+    const encoded = "3:hih";
+    var w = std.Io.Writer.fixed(&buffer);
+    try decode(@TypeOf(encoded), &w, encoded);
+    try expect(std.mem.eql(u8, buffer[0..3], "hih"));
+}
+
+// Test str errors
+
+test "str_wrong_len" {
+    const encoded = "2:hih";
+    var w = std.Io.Writer.fixed(&buffer);
+    const err = decode(@TypeOf(encoded), &w, encoded);
+    try expect(err == Error.WrongLen);
+}
+
+test "str_no_len" {
+    const encoded = ":hih";
+    var w = std.Io.Writer.fixed(&buffer);
+    const err = decode(@TypeOf(encoded), &w, encoded);
+    try expect(err == error.InvalidCharacter);
+}
 
 // Test numbers not close to the max
 test "decode_u8" {
