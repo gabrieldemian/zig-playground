5ab9d32d8a97b3276b6f5463bf82d270f2a544b1
gabrieldemian <gabrielgcr45@gmail.com>
2026-01-07 22:10:29 +0100

    bencode: added arr

--- src/bencode/root.zig
+++ src/bencode/root.zig
@@ -25,9 +25,7 @@ pub const Error = error{
 pub fn decode(
     /// The type to decode `data` into.
     comptime T: type,
-    w: *Writer,
-    /// The string to be decoded as `T.
-    // comptime data: anytype,
+    writer: *Writer,
     reader: *Reader,
 ) !void {
     const info = @typeInfo(T);
@@ -36,7 +34,7 @@ pub fn decode(
         inline .int => |int| {
             _ = int;
             var r = try decode_int(T, reader);
-            _ = try w.write(std.mem.asBytes(&r));
+            _ = try writer.write(std.mem.asBytes(&r));
         },
         // a literal string is a pointer.
         // string = []u8
@@ -49,9 +47,9 @@ pub fn decode(
             }
             var r = try decode_str(reader);
             if (r.len == 0) {
-                _ = try w.writeByte(0);
+                _ = try writer.writeByte(0);
             } else {
-                _ = try w.write(r);
+                _ = try writer.write(r);
             }
         },
         inline .@"struct" => {
@@ -59,26 +57,26 @@ pub fn decode(
             // and call `decode` recursively with the field type.
             var val: T = undefined;
             inline for (std.meta.fields(T)) |f| {
-                @field(val, f.name) = return decode(f.type, w, reader);
+                @field(val, f.name) = return decode(f.type, writer, reader);
             }
         },
         inline .array => |arr| {
-            var r = try decode_arr(arr, T, reader);
-            _ = try w.write(std.mem.asBytes(&r));
+            var r = try decode_arr(arr, reader, writer);
+            _ = try writer.write(std.mem.asBytes(&r));
         },
         else => return Error.NotSupported,
     };
 }
 
 /// Lists are encoded as `l<elements>e`.
-/// l7:bencode3:fooe
+/// For example: `l3:foo:bare`.
 pub fn decode_arr(
     comptime Arr: std.builtin.Type.Array,
-    comptime T: type,
     reader: *Reader,
-) !T {
-    _ = Arr;
+    writer: *Writer,
+) !void {
     const data = reader.buffer[reader.seek..];
+
     if (data.len < 3) {
         return Error.MalformedBuffer;
     }
@@ -89,37 +87,40 @@ pub fn decode_arr(
 
     // empty list
     if (std.mem.eql(u8, data, "le")) {
-        const res: T = std.mem.zeroes(T);
-        return res;
+        return;
     }
 
-    var val: T = undefined;
-    var newdata = data[1..];
-    _ = &newdata;
-    val[0] = 2;
-
-    return val;
+    for (0..Arr.len) |_| {
+        // skip the last byte of the prev structure and enter the current one
+        reader.toss(1);
+        // print(std.fmt.comptimePrint("child: {}\n", .{Arr.child}), .{});
+        _ = try decode(Arr.child, writer, reader);
+    }
 }
 
 /// Decode a byte str, encoded as `<length>:<contents>`.
 /// `<length>` is specified in bytes, not characters.
 /// For example: `6:italia`.
 pub fn decode_str(reader: *Reader) ![]const u8 {
-    const data = reader.buffer[reader.seek..];
-
-    if (data.len < 2) {
+    if (reader.buffer[reader.seek..].len < 2) {
         return Error.MalformedBuffer;
     }
 
     // empty string
-    if (std.mem.eql(u8, data, "0:")) {
+    if (std.mem.eql(u8, reader.buffer[reader.seek..], "0:")) {
         reader.toss(1);
         return "";
     }
 
-    const colon = std.mem.find(u8, data, ":") orelse return Error.MalformedBuffer;
-    const len = try std.fmt.parseInt(usize, data[0..colon], 10);
-    const str = data[colon + 1 .. colon + 1 + len];
+    const colon = std.mem.find(
+        u8,
+        reader.buffer[reader.seek..],
+        ":",
+    ) orelse return Error.MalformedBuffer;
+
+    const len_slice = reader.buffer[reader.seek .. reader.seek + colon];
+    const len = try std.fmt.parseInt(usize, len_slice, 10);
+    const str = reader.buffer[reader.seek + colon + 1 .. reader.seek + colon + len + 1];
     reader.toss(colon + len);
 
     return str;
@@ -132,15 +133,25 @@ pub fn decode_int(
     reader: *Reader,
 ) !T {
     const Int = @typeInfo(T).int;
-    const data = reader.buffer[reader.seek..];
+    // print("reader.buffer[seek..]: {s}\n", .{reader.buffer[reader.seek..]});
 
-    if (data.len < 3) {
+    if (reader.buffer[reader.seek..].len < 3) {
         return Error.MalformedBuffer;
     }
 
-    if (data[0] != 'i' or data[data.len - 1] != 'e') {
+    if (reader.buffer[reader.seek..][0] != 'i') {
         return Error.MalformedBuffer;
     }
+
+    const end = std.mem.find(
+        u8,
+        reader.buffer[reader.seek..],
+        "e",
+    ) orelse return Error.MalformedBuffer;
+    // print("end: {d} seek: {d}\n", .{ end, reader.seek });
+
+    const data = reader.buffer[reader.seek .. reader.seek + end + 1];
+    // print("data: {s}\n", .{data});
 
     if (data[1] == '0' and data.len > 3) {
         return Error.LeadingZero;
@@ -165,16 +176,53 @@ pub fn decode_int(
 }
 
 const expect = std.testing.expect;
-var buffer: [10]u8 = undefined;
+var buffer: [14]u8 = undefined;
 
-test "arr" {
-    const encoded = "li2ee";
+test "arr_int" {
+    const encoded = "li22ee";
     var r = Reader.fixed(encoded);
     const T = [1]u8;
     var w = Writer.fixed(&buffer);
     try decode(T, &w, &r);
-    std.debug.print("my_arr {any}\n", .{buffer});
-    // try expect(buffer[0] == 2);
+    try expect(buffer[0] == 22);
+}
+
+test "arr_int_2" {
+    const encoded = "li22ei34ee";
+    var r = Reader.fixed(encoded);
+    const T = [2]u8;
+    var w = Writer.fixed(&buffer);
+    try decode(T, &w, &r);
+    try expect(buffer[0] == 22);
+    try expect(buffer[1] == 34);
+}
+
+test "arr_str" {
+    const encoded = "l3:vvve";
+    var r = Reader.fixed(encoded);
+    const T = [1]*const [3]u8;
+    var w = Writer.fixed(&buffer);
+    try decode(T, &w, &r);
+    try expect(std.mem.eql(u8, buffer[0..3], "vvv"));
+}
+
+test "arr_str_2" {
+    const encoded = "l3:foo3:bare";
+    var r = Reader.fixed(encoded);
+    const T = [2]*const [3]u8;
+    var w = Writer.fixed(&buffer);
+    try decode(T, &w, &r);
+    try expect(std.mem.eql(u8, buffer[0..3], "foo"));
+    try expect(std.mem.eql(u8, buffer[3..6], "bar"));
+}
+
+test "decode_str" {
+    const encoded = "3:hih";
+    var w = Writer.fixed(&buffer);
+    var r = Reader.fixed(encoded);
+    try decode(@TypeOf(encoded), &w, &r);
+    try expect(std.mem.eql(u8, buffer[0..3], "hih"));
+    try expect(r.seek == encoded.len - 1);
 }
 
 test "struct" {
@@ -195,15 +243,6 @@ test "empty_str" {
     try expect(r.seek == encoded.len - 1);
 }
 
-test "decode_str" {
-    const encoded = "3:hih";
-    var w = Writer.fixed(&buffer);
-    var r = Reader.fixed(encoded);
-    try decode(@TypeOf(encoded), &w, &r);
-    try expect(std.mem.eql(u8, buffer[0..3], "hih"));
-    try expect(r.seek == encoded.len - 1);
-}
-
 test "decode_str_2" {
     const encoded = "10:hihhihhihh";
     var w = Writer.fixed(&buffer);
@@ -213,7 +252,7 @@ test "decode_str_2" {
     try expect(r.seek == encoded.len - 1);
 }
 
-// Test str errors
+// // Test str errors
 
 test "str_wrong_len" {
     // should ignore all the rest of the string
